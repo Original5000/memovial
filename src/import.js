60a3dcd9389b85fc73619a7ea244c58933f68355
Original5000 <usuariodepruebaoriginal5000@gmail.com>
2023-11-12 10:44:58 -0400

    1.3

--- src/import.js
+++ src/import.js
@@ -1,68 +1,382 @@
-import src1 from './images/sub25/ansu-fati.jpg';
-import src2 from './images/sub25/alphonso-davies.jpg';
-import src3 from './images/sub25/christian-pulisic.jpg';
-import src4 from './images/sub25/erling-haaland.jpg';
-import src5 from './images/sub25/jordan-sancho.jpg';
-import src6 from './images/sub25/kylian-mbappe.jpg';
-import src7 from './images/sub25/34-alucinantes.jpg';
+import src1 from './images/sub25/1.jpg';
+import src2 from './images/sub25/2.jpg';
+import src3 from './images/sub25/3.jpg';
+import src4 from './images/sub25/4.jpg';
+import src5 from './images/sub25/5.jpg';
+import src6 from './images/sub25/6.jpg';
+import src7 from './images/sub25/7.jpg';
+import src8 from './images/sub25/8.jpg';
+import src9 from './images/sub25/9.jpg';
+import src10 from './images/sub25/10.jpg';
+import src11 from './images/sub25/11.jpg';
+import src12 from './images/sub25/12.jpg';
+import src13 from './images/sub25/12.jpg';
+import src14 from './images/sub25/14.jpg';
+import src15 from './images/sub25/15.jpg';
+import src16 from './images/sub25/16.jpg';
+import src17 from './images/sub25/17.jpg';
+import src18 from './images/sub25/18.jpg';
+import src19 from './images/sub25/19.jpg';
+import src20 from './images/sub25/20.jpg';
+import src21 from './images/sub25/21.jpg';
+import src22 from './images/sub25/22.jpg';
+import src23 from './images/sub25/23.jpg';
+import src24 from './images/sub25/24.jpg';
+import src25 from './images/sub25/25.jpg';
+import src26 from './images/sub25/26.jpg';
+import src27 from './images/sub25/27.jpg';
+import src28 from './images/sub25/28.jpg';
+import src29 from './images/sub25/29.jpg';
+import src30 from './images/sub25/30.jpg';
+import src31 from './images/sub25/31.jpg';
+import src32 from './images/sub25/32.jpg';
+import src33 from './images/sub25/33.jpg';
+import src34 from './images/sub25/34.jpg';
+import src35 from './images/sub25/35.jpg';
+import src36 from './images/sub25/36.jpg';
+import src37 from './images/sub25/37.jpg';
+import src38 from './images/sub25/38.jpg';
+import src39 from './images/sub25/39.jpg';
+import src40 from './images/sub25/40.jpg';
 
 
 const images = [
   {
     src: src1,
-    player: 'Ansu Fati'
+    player: '1.jpg'
   },
   {
     src: src1,
-    player: 'Ansu Fati'
+    player: '1.jpg'
   },
   {
     src: src2,
-    player: 'Alphonso Davies'
+    player: '2.jpg'
   },
   {
     src: src2,
-    player: 'Alphonso Davies'
+    player: '2.jpg'
   },
   {
     src: src3,
-    player: 'Christian Pulisic'
+    player: '3.jpg'
   },
   {
     src: src3,
-    player: 'Christian Pulisic'
+    player: '3.jpg'
   },
   {
     src: src4,
-    player: 'Erling Haaland'
+    player: '4.jpg'
   },
   {
     src: src4,
-    player: 'Erling Haaland'
+    player: '4.jpg'
   },
   {
     src: src5,
-    player: 'Jordan Sancho'
+    player: '5.jpg'
   },
   {
     src: src5,
-    player: 'Jordan Sancho'
+    player: '5.jpg'
   },
   {
     src: src6,
-    player: 'Kylian Mbappe'
+    player: '6.jpg'
   },
   {
     src: src6,
-    player: 'Kylian Mbappe'
+    player: '6.jpg'
   },
   {
     src: src7,
-    player: '34-alucinantes.jpg'
+    player: '7.jpg'
   },
   {
     src: src7,
-    player: '34-alucinantes.jpg'
+    player: '7.jpg'
+  },
+  {
+    src: src8,
+    player: '8.jpg'
+  },
+  {
+    src: src8,
+    player: '8.jpg'
+  },
+  {
+    src: src9,
+    player: '9.jpg'
+  },
+  {
+    src: src9,
+    player: '9.jpg'
+  },
+  {
+    src: src10,
+    player: '10.jpg'
+  },
+  {
+    src: src10,
+    player: '10.jpg'
+  },{
+    src: src11,
+    player: '11.jpg'
+  },
+  {
+    src: src11,
+    player: '11.jpg'
+  },
+
+  {
+    src: src12,
+    player: '12.jpg'
+  },
+  {
+    src: src12,
+    player: '12.jpg'
+  },
+  {
+    src: src13,
+    player: '13.jpg'
+  },
+  {
+    src: src13,
+    player: '13.jpg'
+  },
+  {
+    src: src14,
+    player: '14.jpg'
+  },
+  {
+    src: src14,
+    player: '14.jpg'
+  },
+  {
+    src: src15,
+    player: '15.jpg'
+  },
+  {
+    src: src15,
+    player: '15.jpg'
+  },
+  {
+    src: src16,
+    player: '16.jpg'
+  },
+  {
+    src: src16,
+    player: '16.jpg'
+  },
+  {
+    src: src17,
+    player: '17.jpg'
+  },
+  {
+    src: src17,
+    player: '17.jpg'
+  },
+  {
+    src: src18,
+    player: '18.jpg'
+  },
+  {
+    src: src18,
+    player: '18.jpg'
+  },
+
+  {
+    src: src19,
+    player: '19.jpg'
+  },
+  {
+    src: src19,
+    player: '19.jpg'
+  },
+
+  {
+    src: src20,
+    player: '20.jpg'
+  },
+  {
+    src: src20,
+    player: '20.jpg'
+  },
+
+  {
+    src: src21,
+    player: '21.jpg'
+  },
+  {
+    src: src21,
+    player: '21.jpg'
+  },
+
+  {
+    src: src22,
+    player: '22.jpg'
+  },
+  {
+    src: src22,
+    player: '22.jpg'
+  },
+
+  {
+    src: src23,
+    player: '23.jpg'
+  },
+  {
+    src: src23,
+    player: '23.jpg'
+  },
+
+  {
+    src: src24,
+    player: '24.jpg'
+  },
+  {
+    src: src24,
+    player: '24.jpg'
+  },
+
+  {
+    src: src25,
+    player: '25.jpg'
+  },
+  {
+    src: src25,
+    player: '25.jpg'
+  },
+
+  {
+    src: src26,
+    player: '26.jpg'
+  },
+  {
+    src: src26,
+    player: '26.jpg'
+  },
+
+  {
+    src: src27,
+    player: '27.jpg'
+  },
+  {
+    src: src27,
+    player: '27.jpg'
+  },
+
+  {
+    src: src28,
+    player: '28.jpg'
+  },
+  {
+    src: src28,
+    player: '28.jpg'
+  },
+
+  {
+    src: src29,
+    player: '29.jpg'
+  },
+  {
+    src: src29,
+    player: '29.jpg'
+  },
+  {
+    src: src30,
+    player: '30.jpg'
+  },
+  {
+    src: src30,
+    player: '30.jpg'
+  },
+
+  {
+    src: src31,
+    player: '31.jpg'
+  },
+  {
+    src: src31,
+    player: '31.jpg'
+  },
+
+  {
+    src: src32,
+    player: '32.jpg'
+  },
+  {
+    src: src32,
+    player: '32.jpg'
+  },
+
+  {
+    src: src33,
+    player: '33.jpg'
+  },
+  {
+    src: src33,
+    player: '33.jpg'
+  },
+
+  {
+    src: src34,
+    player: '34.jpg'
+  },
+  {
+    src: src34,
+    player: '34.jpg'
+  },
+
+  {
+    src: src35,
+    player: '35.jpg'
+  },
+  {
+    src: src35,
+    player: '35.jpg'
+  },
+  {
+    src: src36,
+    player: '36.jpg'
+  },
+  {
+    src: src36,
+    player: '36.jpg'
+  },
+
+  {
+    src: src37,
+    player: '37.jpg'
+  },
+  {
+    src: src37,
+    player: '37.jpg'
+  },
+  {
+    src: src38,
+    player: '38.jpg'
+  },
+  {
+    src: src38,
+    player: '38.jpg'
+  },
+  {
+    src: src39,
+    player: '39.jpg'
+  },
+  {
+    src: src39,
+    player: '39.jpg'
+  },
+  {
+    src: src40,
+    player: '40.jpg'
+  },
+  {
+    src: src40,
+    player: '40.jpg'
   }
 
 ];
